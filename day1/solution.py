9993312df30af238bda582bb357dc903a2f0daa7
Tiernan Garsys <tgarsys@mac.com>
2021-12-01 17:14:57 -0500

    Implemented day 1 solution

---
+++ day1/solution.py
@@ -0,0 +1,26 @@
+# Advent of Code 2021, Day 1
+
+import sys
+
+def run():
+    if len(sys.argv) < 3:
+        raise ValueError('No arguments provided')
+
+    filename = sys.argv[1]
+    window_size = int(sys.argv[2])
+
+    depths = []
+
+    with open(filename) as file:
+        depths = [int(depth) for depth in file.readlines()]
+
+    segments = []
+    for idx in range(0, len(depths) - window_size):
+        segments.append(sum(depths[idx:idx+window_size+1]))
+
+    increases = sum(segments[idx] > segments[idx-1] for idx in range(1, len(segments)))
+
+    print('Depth data contains %s increases' % increases)
+
+if __name__ == '__main__':
+    run()
