384d1f8551063e112d315d7700d00d78d46ac859
Tiernan Garsys <tgarsys@mac.com>
2021-12-08 00:44:06 -0500

    complete day 7, part A

---
+++ day7/solution_a.py
@@ -0,0 +1,21 @@
+# Advent of Code 2023, Day 7, Solution A
+
+import functools
+import sys
+
+
+def run():
+    if len(sys.argv) < 2:
+        raise ValueError('No arguments provided. Usage: solution.py input_file days')
+
+    filename = sys.argv[1]
+
+    positions = []
+    with open(filename) as f:
+        positions = [int(p) for p in f.read().split(',')] 
+
+    costs = [functools.reduce(lambda a, p: a + abs(pos - p), positions, 0) for pos in positions]
+    print('The answer is %i' % min(costs))
+
+if __name__ == '__main__':
+    run()
